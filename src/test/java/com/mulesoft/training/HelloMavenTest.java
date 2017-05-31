d8a25109ea66e92167551b68cc2553727d9de29c
William Chronowski <WC47833@pekininsurance.com>
2017-05-31 12:41:50 -0500

    Add test to define content type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -16,6 +17,14 @@ public class HelloMavenTest extends FunctionalTestCase {
     public void mavenFlowReturnsHelloMaven() throws Exception {
     	System.out.print("\n\nDynamic Http Port used in JUnit Test Case #1 --------> " + dynamicPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	System.out.print("\n\nDynamic Http Port used in JUnit Test Case #2 --------> " + dynamicPort.getNumber() + "\n\n");
+    	MuleEvent event = runFlow("retrieveFlights");
+    	String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    	assertEquals("application/json", contentType);
     }
     
     @Override
